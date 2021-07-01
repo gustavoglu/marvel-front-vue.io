8161d3b4042b0f86434bbad5804edc0ead14b7a5
Gustavo <gustavo.giroldo@accist.com.br>
2021-07-01 13:06:25 -0300

    deploy 6

--- precache-manifest.00f186a8945290da3a23dac29979137a.js
+++ precache-manifest.00f186a8945290da3a23dac29979137a.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "8d3c9f62086bb5cd3042",
-    "url": "/css/chunk-vendors.1b1af926.css"
+    "url": "css/chunk-vendors.1b1af926.css"
   },
   {
     "revision": "fd864f3e90cdabcfd7d0c4401078b6e7",
-    "url": "/img/logo.fd864f3e.png"
+    "url": "img/logo.fd864f3e.png"
   },
   {
-    "revision": "edf70ea916673bb0a9f9838eb6e58908",
-    "url": "/index.html"
+    "revision": "2c93cd473ae949e7e29e4c483581ed93",
+    "url": "index.html"
   },
   {
-    "revision": "91e0992b9e084d906650",
-    "url": "/js/app.8a8c95c8.js"
+    "revision": "2ed68f448b171f7f4588",
+    "url": "js/app.c8fd4d30.js"
   },
   {
     "revision": "8d3c9f62086bb5cd3042",
-    "url": "/js/chunk-vendors.3c28eba9.js"
+    "url": "js/chunk-vendors.3c28eba9.js"
   },
   {
     "revision": "1efcfad4dc70329c4839f17d7e6f2a3e",
-    "url": "/manifest.json"
+    "url": "manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "url": "robots.txt"
   }
 ]);
