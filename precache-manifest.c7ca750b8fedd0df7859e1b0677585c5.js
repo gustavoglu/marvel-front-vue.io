c0db76365933d8c5caba138e00cd49c0f361ec6f
Gustavo <gustavo.giroldo@accist.com.br>
2021-07-01 12:02:30 -0300

    deploy 1

---
+++ precache-manifest.c7ca750b8fedd0df7859e1b0677585c5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d3c9f62086bb5cd3042",
+    "url": "/css/chunk-vendors.1b1af926.css"
+  },
+  {
+    "revision": "fd864f3e90cdabcfd7d0c4401078b6e7",
+    "url": "/img/logo.fd864f3e.png"
+  },
+  {
+    "revision": "e2042127d5364b9181a6aac1b591085f",
+    "url": "/img2/_head.html"
+  },
+  {
+    "revision": "fb9e90e263bc688c46d28a96199e8c21",
+    "url": "/img2/android-144x144.png"
+  },
+  {
+    "revision": "09bc8bdaeb4a65a4888475e4a6034db2",
+    "url": "/img2/android-192x192.png"
+  },
+  {
+    "revision": "a6155cba7a8d0e77fd7a1bd68ea7c944",
+    "url": "/img2/android-36x36.png"
+  },
+  {
+    "revision": "746d02459e590b2f5eb6ef832cafea81",
+    "url": "/img2/android-48x48.png"
+  },
+  {
+    "revision": "dff2a3100a7ad2802a19ce2713c9775c",
+    "url": "/img2/android-72x72.png"
+  },
+  {
+    "revision": "8cc7d02ebab7c6300b6f3a9ea3baa740",
+    "url": "/img2/android-96x96.png"
+  },
+  {
+    "revision": "4fbc7527696a5cd9ccf5dc1ae542a5e1",
+    "url": "/img2/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "434a78130f7aaf76db8a90e4467a88a4",
+    "url": "/img2/browserconfig.xml"
+  },
+  {
+    "revision": "edfdb7ffce8361d8f99bc069b4201a30",
+    "url": "/img2/favicon-16x16.png"
+  },
+  {
+    "revision": "d6d8e7ac8631a2a260d55c24ea9290b0",
+    "url": "/img2/favicon-32x32.png"
+  },
+  {
+    "revision": "dce54c1b9e0639a9deeccadac3ac3780",
+    "url": "/img2/pwa-192x192.png"
+  },
+  {
+    "revision": "9c962abbaa202cb90649bf63d024a2d2",
+    "url": "/img2/pwa-512x512.png"
+  },
+  {
+    "revision": "0d8c59524d6df7649ab4601c4971fc56",
+    "url": "/img2/tile150x150.png"
+  },
+  {
+    "revision": "4ed15dab86ca1482d54deafda283e9a4",
+    "url": "/img2/tile310x150.png"
+  },
+  {
+    "revision": "9d39a65541bcd0d46f704f675891f0d7",
+    "url": "/img2/tile310x310.png"
+  },
+  {
+    "revision": "8117a41e7b7c6a5e47a3f60084c1062a",
+    "url": "/img2/tile70x70.png"
+  },
+  {
+    "revision": "d27028a1085951b466dfa71d570c88b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b4a5c25a9512139577",
+    "url": "/js/app.c8d76f2e.js"
+  },
+  {
+    "revision": "8d3c9f62086bb5cd3042",
+    "url": "/js/chunk-vendors.3c28eba9.js"
+  },
+  {
+    "revision": "1efcfad4dc70329c4839f17d7e6f2a3e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
